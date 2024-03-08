8389b158a28abbc945e7ea3e5a0b8ecaa9aa2ed8
Wojtek <kantor.wojtek2002@gmail.com>
2024-03-08 20:03:47 +0100

    Add files via upload

---
+++ Projekt/Uzytkownik.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    public abstract class Uzytkownik
+    {
+        string imie;
+        string nazwisko;
+
+        public string Imie { get => imie; set => imie = value; }
+        public string Nazwisko { get => nazwisko; set => nazwisko = value; }
+
+        public Uzytkownik()
+        {
+
+        }
+        public Uzytkownik(string imie, string nazwisko)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+        }  
+    }
+}
